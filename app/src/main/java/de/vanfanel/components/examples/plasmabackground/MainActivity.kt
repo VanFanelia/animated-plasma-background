b85ddb0164e95562dac1ccb60013c7e7d0c8c50a
Mathias Wegener <wegener@sipgate.de>
2022-01-07 15:45:48 +0100

    :sparkles: Adds menu to demo to enabble user to experiement with settings
    Co-authored-by: Van Fanel <van@gmx-topmail.de>

--- app/src/main/java/de/vanfanel/components/examples/plasmabackground/MainActivity.kt
+++ app/src/main/java/de/vanfanel/components/examples/plasmabackground/MainActivity.kt
@@ -3,16 +3,43 @@ package de.vanfanel.components.examples.plasmabackground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.vanfanel.components.examples.plasmabackground.ui.theme.AnimatedPlasmaBackgroundTheme
+import de.vanfanel.components.examples.plasmabackground.ui.theme.TRANSPARENT_GREY
 import de.vanfanel.components.plasmabackground.PlasmaBackground
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +48,59 @@ class MainActivity : ComponentActivity() {
                 Box(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    PlasmaBackground()
+                    var showMenuOverlay by remember { mutableStateOf(false) }
+                    val showFPS = remember { mutableStateOf(false) }
+
+                    PlasmaBackground(
+                        debugShowFPS = showFPS.value
+                    )
+
+                    AnimatedVisibility(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .align(Alignment.BottomCenter),
+                        visible = showMenuOverlay,
+                        enter = slideInVertically({ it }),
+                        exit = slideOutVertically({ it })
+                    ) {
+                        Surface(
+                            color = TRANSPARENT_GREY
+                        ) {
+                            Column(modifier = Modifier.padding(16.dp)) {
+                                Row(
+                                    modifier = Modifier.clickable {
+                                        showFPS.value = !showFPS.value
+                                    }
+                                ) {
+                                    Checkbox(
+                                        checked = showFPS.value,
+                                        onCheckedChange = { showFPS.value = it }
+                                    )
+                                    Text(
+                                        modifier = Modifier.padding(start = 20.dp),
+                                        text = "Show FPS"
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    Button(
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(16.dp)
+                            .size(width = 48.dp, height = 48.dp),
+                        shape = RoundedCornerShape(99.dp),
+                        elevation = ButtonDefaults.elevation(0.dp, 0.dp, 0.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = TRANSPARENT_GREY),
+                        onClick = {
+                            showMenuOverlay = !showMenuOverlay
+                        },
+                    ) {
+                        val image: Painter = painterResource(id = R.drawable.ic_menu)
+                        Image(painter = image, contentDescription = "")
+                    }
                 }
             }
         }
--- app/src/main/java/de/vanfanel/components/examples/plasmabackground/ui/theme/Theme.kt
+++ app/src/main/java/de/vanfanel/components/examples/plasmabackground/ui/theme/Theme.kt
@@ -22,6 +22,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val TRANSPARENT_GREY = Color(0xaaaaaaaa)
 
 /*****
  * Shapes
